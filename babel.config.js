4d1a92559add4aef7bf1152d4b0d3cf8ebde918d
maleich <mleich@athenian.org>
2022-08-13 13:47:22 -0700

    revert config

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
-  prism: {
-    theme: lightCodeTheme,
-    darkTheme: darkCodeTheme,
-  },
   themes: ['@docusaurus/theme-live-codeblock'],
 };
